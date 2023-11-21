de9ea87eafd1fad2f054e418b86f89f63b8844b3
KawaiNime Pro <147604824+kawainime@users.noreply.github.com>
2023-11-22 02:48:51 +0700

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 )
 
 func main() {
-
 	file, err := os.ReadFile("feed.yaml")
 	if err != nil {
 		fmt.Println("Error reading config file: ", err)
+		return
 	}
 
 	data := extractor.GetFeedURL(file)
@@ -22,7 +22,6 @@ func main() {
 	for _, FeedURL := range data {
 		postParser(FeedURL)
 	}
-
 }
 
 func postParser(FeedURL string) {
@@ -31,7 +30,6 @@ func postParser(FeedURL string) {
 	feed, _ := fp.ParseURL(FeedURL)
 
 	for _, item := range feed.Items {
-
 		NowTime := time.Now()
 		ParsedNowTime := time.Unix(NowTime.Unix(), 0)
 
@@ -39,27 +37,23 @@ func postParser(FeedURL string) {
 		ParsedPublishedTime := time.Unix(PublishedTime.Unix(), 0)
 
 		if ParsedNowTime.Sub(ParsedPublishedTime).Hours() < 24 {
-
 			PostTitle := item.Title
 			PostLink := item.Link
 			PostAuthor := item.Author.Name
 			Category := item.Categories
 
-			hashtags, _ := getHastags(Category)
+			hashtags, _ := getHashtags(Category)
 
 			Tweet := fmt.Sprintf("%s was published by %s  🎉🎉🎉 \n %s \n %s", PostTitle, PostAuthor, hashtags, PostLink)
 
-			TweeetId, _ := platform.PublishToTwitter(Tweet)
+			TweeetId, _ := platforms.PublishToTwitter(Tweet)
 
 			fmt.Printf("%s with the TweetID: %s was posted! ✔️ \n ", PostTitle, TweeetId)
-
 		}
-
 	}
 }
 
-func getHastags(Categories []string) (string, error) {
-
+func getHashtags(Categories []string) (string, error) {
 	var Category string
 
 	for _, element := range Categories {
